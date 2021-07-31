716f3a77231a4eefe0e8ec31e746511ea999c895
Thiago Tassinari <thiagotassinari12@gmail.com>
2021-07-31 01:16:14 -0300

    revert: inserir duas funções para dinâmicamente instanciar as propriedades CSS

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -25,6 +25,8 @@ export const Button = styled.button`
     opacity: 1;
     border-radius: 0.8rem;
     ${({ghost}) => (ghost ? ButtonGhost : ButtonDefault)}; // Utilizando operador ternário
+    transition: opacity ${({ theme }) => theme.transition};
+    border-radius: ${(props) => props.theme.borderRadius};
     &:hover,
     &:focus {
         opacity: .5;
